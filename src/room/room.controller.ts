29633ce19bd9f445935ea50361668e2be23b55ec
vcky4 <okon.victor.u@gmail.com>
2024-05-28 09:22:49 +0100

    feet: set up swagger UI

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { RoomDTO } from './dto/room.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('room')
+@ApiTags('room')
 export class RoomController {
     constructor(private serv: RoomService) { }
 
     @Get()
+    @ApiResponse({ type: [RoomDTO] })
     public async getAll(): Promise<RoomDTO[]> {
-      return await this.serv.getAll();
+        return await this.serv.getAll();
     }
 }
